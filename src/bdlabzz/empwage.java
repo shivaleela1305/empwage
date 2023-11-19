098d7281b6bcc1fe6be1379866d29cbb9ee9936a
Shivaleela <shivaleelakatti13@gmail.com>
2023-11-19 13:37:16 +0530

    added java functionality for employee wage attendance check

--- src/bdlabzz/empwage.java
+++ src/bdlabzz/empwage.java
@@ -4,7 +4,13 @@ public class empwage {
 
 	public static void main(String[] args) {
 		System.out.println("welcome to employee wage program");
-
+		int IS_FULL_TIME = 1;
+        double check = Math.floor(Math.random()*10) % 2;
+        if(IS_FULL_TIME == check){
+            System.out.println("Employee is present");
+        }else{
+            System.out.println("employee is absent");
+        }
 	}
 
 }

bda4f0760710e7d42fbec35b53d13ffb11bf5a76
Shivaleela <shivaleelakatti13@gmail.com>
2023-11-19 16:59:54 +0530

    added java functionality for employee monthly wage check

--- src/bdlabzz/empwage.java
+++ src/bdlabzz/empwage.java
@@ -72,6 +72,26 @@ public class empwage {
         
         int wage11 = workingHours11 * WAGE_PER_HR11;
         System.out.println("Employee Daily Wage is " + wage11);
+        
+        final int PART_TIME11 = 1;
+        final int FULL_TIME111 = 2;
+        final int WAGE_PER_HR111 = 20;
+        final int Working_Days = 20;
+
+        int totalWage = 0;
+        for(int i = 1;i<Working_Days;i++){
+            int empType111 = (int) (Math.random() * 100) % 3;
+            int workingHours111 = 0;
+            switch (empType111) {
+                case FULL_TIME111: workingHours111 = 8;
+                case PART_TIME11: workingHours111 = 4;
+            
+                default: workingHours111 = 0;
+            }
+            int wage111 = workingHours111 * WAGE_PER_HR111;
+            totalWage = totalWage + wage111;
+        }
+        System.out.println("total wage of the month is " + totalWage);
                 
 	}
 
